a49406a2c758cfc7d6b58bffdeb02c266fddb7c2
Birchi <noreply@bi.code>
2023-04-30 15:01:24 +0200

    Initial release

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#####################################################################
+#
+# Copyright (c) 2023-present, Birchi (https://github.com/Birchi)
+# All rights reserved.
+#
+# This source code is licensed under the MIT license.
+#
+#####################################################################
+##
+# Main
+##
+echo "This is a test!"
